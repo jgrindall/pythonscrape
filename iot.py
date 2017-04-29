a665a2c1a05ac439adfc705be4e9eb593ec935c8
john.grindall@gmail.com <john.grindall@gmail.com>
2017-04-29 11:57:16 +0100

    adding pages

--- iot.py
+++ iot.py
@@ -4,8 +4,9 @@ import urllib
 import re
 
 BLOCK_SIZE = 8192
-
 LINK_RE = re.compile('http://open.live.bbc.co.uk/mediaselector/5/redir/version/2.0/mediaset/audio-nondrm-download-low/proto/http/vpid/(.*)\.mp3')
+MAX_PAGE_NUM = 26
+BASE_URL = 'http://www.bbc.co.uk/programmes/b006qykl/episodes/downloads';
 
 def downloadMp3(url):
 	print(url)
@@ -21,13 +22,18 @@ def downloadMp3(url):
 		f.write(buffer),
 	f.close()
 
+def downloadAllOnPage(num):
+	url = BASE_URL
+	if num >= 2:
+		url += ("?page=" + num)
+	print("page" + url)
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, "lxml")
+	for link in soup.find_all('a'):
+		url = link.get('href')
+		if re.search(LINK_RE, url):
+			downloadMp3(url)
 
 
-url = 'http://www.bbc.co.uk/programmes/b006qykl/episodes/downloads';
-r  = requests.get(url)
-soup = BeautifulSoup(r.text, "lxml")
-for link in soup.find_all('a'):
-	url = link.get('href')
-	if re.search(LINK_RE, url):
-		downloadMp3(url)
-
+for i in range(1, MAX_PAGE_NUM):
+   downloadAllOnPage(i)
